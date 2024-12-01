2ef8966ec779b7a66a6bbd90466b77661dacf9cc
ViniciusBertonii <vinicius.marcos98@edu.pucrs.br>
2024-12-01 16:34:17 -0300

    classes de comanda criadas

--- src/main/java/br/pucrs/poo/controller/ClienteController.java
+++ src/main/java/br/pucrs/poo/controller/ClienteController.java
@@ -1,10 +1,51 @@
 package br.pucrs.poo.controller;
 import br.pucrs.poo.dto.ClienteDTO;
 import br.pucrs.poo.service.ClienteService;
-import lombok.RequiredArgsConstructor;
-@RequiredArgsConstructor
+import br.pucrs.poo.view.ClienteView;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.Scanner;
+
+@AllArgsConstructor
 public class ClienteController {
     private final ClienteService clienteService;
+    private ClienteView view;
+    private Scanner scanner;
+
+    public void iniciarInterface() {
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\n=== Sistema de Clientes ===");
+            System.out.println("1. Cadastrar Cliente");
+            System.out.println("2. Listar Clientes");
+            System.out.println("0. Sair");
+            System.out.print("Escolha uma opção: ");
+            int opcao = scanner.nextInt();
+            scanner.nextLine(); // Limpar buffer
+
+            switch (opcao) {
+                case 0:
+                    running = false;
+                    System.out.println("Sistema encerrado!");
+                    break;
+                case 1:
+                    criarCliente();
+                    break;
+                case 2:
+                    listarClientes();
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        }
+    }
+
+    public List <ClienteDTO> listarClientes() {
+       return clienteService.listarClientes();
+    }
+    
     public ClienteDTO criarCliente(ClienteDTO cliente) {
         return clienteService.cadastrarCliente(cliente);
     }
--- src/main/java/br/pucrs/poo/service/ClienteService.java
+++ src/main/java/br/pucrs/poo/service/ClienteService.java
@@ -1,18 +1,32 @@
 package br.pucrs.poo.service;
+
 import br.pucrs.poo.dto.ClienteDTO;
 import br.pucrs.poo.entity.Cliente;
 import br.pucrs.poo.mapper.ClienteMapper;
 import br.pucrs.poo.repository.ClienteRepository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class ClienteService {
     private ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
     public ClienteDTO cadastrarCliente(ClienteDTO cliente) {
         Optional<Cliente> byCpf = clienteRepository.findByCpf(cliente.cpf());
-        if (byCpf.isPresent()) throw new RuntimeException("placeholder");
+        if (byCpf.isPresent()) throw new RuntimeException("Cliente com esse CPF já cadastrado!");
         Cliente entity = ClienteMapper.INSTANCE.toCliente(cliente);
         Cliente save = clienteRepository.save(entity);
         return ClienteMapper.INSTANCE.toClienteDTO(save);
+    }
+
+    public List<ClienteDTO> listarClientes() {
+        return clienteRepository.findAll()
+                .map(ClienteMapper.INSTANCE::toClienteDTO)
+                .collect(Collectors.toList());
     }
 }
