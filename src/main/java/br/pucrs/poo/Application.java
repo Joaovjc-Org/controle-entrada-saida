0d2aa9412479a365d7ac3602cbfffcd50746eaa3
João Victor <74030829+joaovjc@users.noreply.github.com>
2024-12-02 11:08:59 -0300

    adicionando Tela inicial em swing e criando telas base para cliente, folha e item

--- src/main/java/br/pucrs/poo/Application.java
+++ src/main/java/br/pucrs/poo/Application.java
@@ -1,10 +1,14 @@
 package br.pucrs.poo;
+import br.pucrs.poo.view.TelaPrincipalFrame;
 import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.swing.*;
+
 @SpringBootApplication
 @EnableJpaRepositories
 public class Application implements CommandLineRunner {
@@ -17,5 +21,6 @@ public class Application implements CommandLineRunner {
     }
     @Override
     public void run(String... args) {
+        SwingUtilities.invokeLater(() -> TelaPrincipalFrame.startGUI(args));
     }
 }
--- src/main/java/br/pucrs/poo/controller/ClienteController.java
+++ src/main/java/br/pucrs/poo/controller/ClienteController.java
@@ -7,13 +7,10 @@ import org.springframework.stereotype.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 @Component
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class ClienteController implements ActionListener {
-    private final ClienteService clienteService;
+public class ClienteController{
+    @Autowired
+    private ClienteService clienteService;
     public ClienteDTO criarCliente(ClienteDTO cliente) {
         return clienteService.cadastrarCliente(cliente);
-    }
-    @Override
-    public void actionPerformed(ActionEvent e) {
     }
 }
